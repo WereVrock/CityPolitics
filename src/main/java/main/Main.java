0e33ae6ff3e6a5bb32c0a8902bfb5d1a75a0042f
bugrabeyaz <bugrabeyaz@yahoo.com>
2026-04-26 17:17:40 +0300

    map error fixed

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -11,6 +11,7 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
+         System.setProperty("sun.java2d.uiScale", "1");
         SwingUtilities.invokeLater(() -> {
             try {
                 UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
