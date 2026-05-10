c03ecb82d6174c2acaeb167d26ad1e1328f3bada
bugrabeyaz <bugrabeyaz@yahoo.com>
2026-05-10 22:32:00 +0300

    set pop inf gain to 0

--- src/main/java/main/parameters/GameParameters.java
+++ src/main/java/main/parameters/GameParameters.java
@@ -38,22 +38,22 @@ public final class GameParameters {
     // =========================================================
     public static final double HUMAN_FOOD_CONSUMPTION       = 1.0;
     public static final double HUMAN_MONEY_GENERATION       = 1.2;
-    public static final double HUMAN_INFLUENCE_GENERATION   = 0.8;
+    public static final double HUMAN_INFLUENCE_GENERATION   = 0.0;
     public static final double HUMAN_MANPOWER_CONTRIBUTION  = 1.0;
 
     public static final double DWARF_FOOD_CONSUMPTION       = 1.2;
     public static final double DWARF_MONEY_GENERATION       = 2.0;
-    public static final double DWARF_INFLUENCE_GENERATION   = 0.4;
+    public static final double DWARF_INFLUENCE_GENERATION   = 0.0;
     public static final double DWARF_MANPOWER_CONTRIBUTION  = 1.3;
 
     public static final double ORC_FOOD_CONSUMPTION         = 1.8;
     public static final double ORC_MONEY_GENERATION         = 0.6;
-    public static final double ORC_INFLUENCE_GENERATION     = 0.3;
+    public static final double ORC_INFLUENCE_GENERATION     = 0.0;
     public static final double ORC_MANPOWER_CONTRIBUTION    = 2.0;
 
     public static final double ELF_FOOD_CONSUMPTION         = 0.6;
     public static final double ELF_MONEY_GENERATION         = 0.9;
-    public static final double ELF_INFLUENCE_GENERATION     = 2.5;
+    public static final double ELF_INFLUENCE_GENERATION     = 0.0;
     public static final double ELF_MANPOWER_CONTRIBUTION    = 0.7;
 
     // =========================================================
